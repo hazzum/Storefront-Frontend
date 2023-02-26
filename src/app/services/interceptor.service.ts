bac0bf8ae937a8055e9c7584a7f262b71047b6d9
hazzum <hazem.mix2012@gmail.com>
2023-02-26 14:05:05 +0200

    rudimentary http error handling

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -3,20 +3,38 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { StorageService } from './storage.service';
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, public storage: StorageService) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
         Authorization: `Bearer ${this.auth.getToken()}`
       }
     });
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = error.error.message
+        } else {
+          // server-side error
+          if(error.status==401 || error.status==403) {
+            this.storage.clean()
+            window.location.reload()
+          }
+          errorMessage = error.message
+        }
+        return throwError(()=> new Error(errorMessage));
+      })
+    )
   }
 }
